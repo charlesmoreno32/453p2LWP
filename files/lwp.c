605441f32dcb126fd469b6ce417452af92b6ffb5
Matthew <mattbosio08@gmail.com>
2024-04-26 15:39:55 -0700

    Kinda got lwp.c working

--- files/lwp.c
+++ files/lwp.c
@@ -42,7 +42,7 @@ int NUM_THREADS = 0;
 thread currThread = NULL;
 
 
-static void lwp_wrap(lwpfun fun, void *arg){
+void lwp_wrap(lwpfun fun, void *arg){
 	// Call the given lwpfunction with the given argument.
 	// Calls lwp_exit() with its return value
 	int rval;
@@ -54,11 +54,10 @@ tid_t lwp_create(lwpfun function, void *argument){
 	// Creates a new thread and admits it to the current scheduler. The thread’s resources will consist of a
 	// context and stack, both initialized so that when the scheduler chooses this thread and its context is
 	// loaded via swap_rfiles() it will run the given function. This may be called by any thread.
-	unsigned long *stack, *original_stack;
 	long page_size = sysconf(_SC_PAGE_SIZE);
 	struct rlimit rlim;
-	size_t howbig, soft_limit, default_size, stack_size;
-
+	size_t soft_limit, default_size, stack_size;
+	ssize_t howbig = 0;
 	thread new_thread = (thread)malloc(sizeof(struct threadinfo_st));
 
 	NUM_THREADS++;
@@ -90,36 +89,38 @@ tid_t lwp_create(lwpfun function, void *argument){
 		stack_size = ((default_size / page_size) + 1) * page_size;
 		howbig = stack_size;
 	}
-	new_thread->stacksize = howbig;
-	// allocate stack
-	stack = (unsigned long *)mmap(NULL, howbig + 15, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_STACK, -1, 0 );
-
-
-	original_stack = stack;
 	
-	printf("%p\n", (void *)stack);
-	//go to top of stack
-	stack += howbig - 1;
-	stack = (unsigned long *)(((uintptr_t)stack + 15) & ~(uintptr_t)0x0F);
-
-	printf("%p\n", (void *)stack);
-
-	new_thread->stack = stack;
-
-	//set the stack pointer
-	new_thread->state.rsp = (unsigned long)stack;
-
-	// add lwp_wrap to stack
-	new_thread->state.rsp --;
-	new_thread->state.rsp = (unsigned long)lwp_wrap;
-
-	// set base pointer to lwp_wrap where function is stored
-	new_thread->state.rbp = (unsigned long)stack;
+	// allocate stack
+	new_thread->stack = (unsigned long *)mmap(NULL, (howbig * sizeof(unsigned long)), PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_STACK, -1, 0 );
+	
+	new_thread->stacksize = howbig;
+	unsigned long *stack_pointer;
+	unsigned long *base_pointer;
 
 	// load remaining registers
 	new_thread->state.rdi = (unsigned long) function;
 	new_thread->state.rsi = (unsigned long) argument;
 	new_thread->state.fxsave = FPU_INIT;
+
+	//go to top of stack
+	stack_pointer = (new_thread->stack + new_thread->stacksize-1);
+	stack_pointer = (unsigned long*)(((uintptr_t)stack_pointer + 15) & ~0xF);
+	base_pointer = stack_pointer;
+
+	//set the stack pointer
+	
+	stack_pointer--;
+	*stack_pointer = (unsigned long)lwp_exit;
+	// add lwp_wrap to stack
+	stack_pointer--;
+    *stack_pointer = (unsigned long)lwp_wrap;
+	
+	stack_pointer--;
+    *stack_pointer = (unsigned long)base_pointer;
+	
+	// set base pointer to lwp_wrap where function is stored
+	new_thread->state.rbp = (unsigned long)(stack_pointer);
+	new_thread->state.rsp = (unsigned long)(stack_pointer);
 
 	// set thread status to live
 	new_thread->status = LWP_LIVE;
@@ -142,9 +143,8 @@ void lwp_start(void){
 	original->stack = NULL;
 	scheduler currentScheduler = lwp_get_scheduler();
 	currentScheduler->admit(original);
-
 	// set current thread
-	currThread = currentScheduler->next();
+	//currThread = currentScheduler->next();
 	
 	// yield to whatever scheduler decides
 	lwp_yield();
@@ -159,6 +159,12 @@ void lwp_yield(void){
 	
 	// get next thread
 	thread next_thread = lwp_get_scheduler()->next();
+	if(next_thread){
+		printf("%d\n", next_thread->tid);
+	} else{
+		printf("NULL");
+	}
+	
 	
 	// load context of next thread
 	
@@ -167,14 +173,18 @@ void lwp_yield(void){
 	//	lwp_exit(currThread->status);
 	//}
 
-	swap_rfiles(&(currThread->state), &(next_thread->state));
-	
+	if(currThread){
+		swap_rfiles(&(currThread->state), &(next_thread->state));
+	}else{
+		swap_rfiles(NULL, &(next_thread->state));
+	}
 };
 
 void lwp_exit(int exitval){
 	// Terminates the calling thread. Its termination status becomes the low 8 bits of the passed integer. The
  	// thread’s resources will be deallocated once it is waited for in lwp_wait(). Yields control to the next
 	// thread using lwp_yield()
+	lwp_yield();
 };
 
 tid_t lwp_wait(int *status){
@@ -193,7 +203,7 @@ tid_t lwp_gettid(void){
 thread tid2thread(tid_t tid){
 	// returns the thread corresponding to the given thread ID
 	// or NULL if the ID is invalid
-	thread head = 
+	//thread head = 
 	return NULL;
 }
 
@@ -204,3 +214,4 @@ void lwp_set_scheduler(scheduler sched){
 scheduler lwp_get_scheduler(void){
 	return currScheduler;
 };
+
